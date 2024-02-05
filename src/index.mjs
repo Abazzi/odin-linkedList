7f07aabf095e4a5656411fdf558210f633850abe
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2024-02-05 16:40:38 -0500

    Changed index.js to index.mjs to use in cli

---
+++ src/index.mjs
@@ -0,0 +1,10 @@
+import LinkedList from './LinkedList.mjs';
+
+const list = new LinkedList();
+
+list.append(1);
+list.append(2);
+list.append(3);
+list.append(4);
+list.prepend(5);
+console.log(list);
